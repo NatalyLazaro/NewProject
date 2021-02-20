8ae5c33a8da70b71de797d3c9320afcdbec7cef2
Marleny Garrido Lazaro <74108713+mArlettGL@users.noreply.github.com>
2021-02-19 22:51:04 -0600

    Aqui vamso

---
+++ src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo;
+
+/**
+ *
+ * @author arlet
+ */
+public class Hola_mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println(" ═════════════════");
+        System.out.println("║  Bienvenido a Progra II  ║");
+        System.out.println(" ═════════════════ \n");
+        System.out.println("\tc(¯¯). .(¯¯)3 \n");
+        System.out.println("═══════");
+        System.out.println("| ##  ##  |");
+        System.out.println("| ##  ##  |");
+        System.out.println("|___|¨|___|");
+    }    
+}
